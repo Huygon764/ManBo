8b84c33d4793c745d11ad06f9732c8440f692f6c
BoH <115441679+Huygon764@users.noreply.github.com>
2023-05-15 23:53:23 +0700

    Add files via upload

---
+++ Login/sign.js
@@ -0,0 +1,27 @@
+function darkmode() {
+    document.getElementsByClassName('container_main')[0].classList.toggle("background_content");
+    document.getElementsByClassName('nav')[0].classList.toggle("background_nav");
+    document.getElementsByClassName('content')[0].classList.toggle("dark");
+    
+    const length_name_item = document.getElementsByClassName('form_sign_in').length;
+
+    const name_item = document.getElementsByClassName('form_sign_in');
+
+    for (let j = 0; j < length_name_item; j++) 
+    {
+        name_item[j].classList.toggle("white");
+    }
+
+}
+
+function onclickMenu()
+{
+    var menu = document.querySelector(".nav");
+    if (menu.style.display == "none")
+    {
+        menu.style.display = "block";
+    }
+    else {
+        menu.style.display = "none";
+    }
+}
